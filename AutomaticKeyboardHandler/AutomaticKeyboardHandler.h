3cf41b7919cd3c44901d71849ea5fb4c843cb917
srpunni <srpunni@gmail.com>
2017-09-27 13:57:32 +0530

    first

---
+++ AutomaticKeyboardHandler/AutomaticKeyboardHandler.h
@@ -0,0 +1,19 @@
+//
+//  AutomaticKeyboardHandler.h
+//  AutomaticKeyboardHandler
+//
+//  Created by Sukhpal.singh on 9/27/17.
+//  Copyright © 2017 Landmark Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutomaticKeyboardHandler.
+FOUNDATION_EXPORT double AutomaticKeyboardHandlerVersionNumber;
+
+//! Project version string for AutomaticKeyboardHandler.
+FOUNDATION_EXPORT const unsigned char AutomaticKeyboardHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutomaticKeyboardHandler/PublicHeader.h>
+
+
